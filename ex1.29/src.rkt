92e276cf3998395cb7a0ba6b0c55b3426bbf9d60
Nikolay <northernnikolay@gmail.com>
2023-06-20 21:33:50 +0300

    Add description to exz

--- ex1.29/src.rkt
+++ ex1.29/src.rkt
@@ -1,3 +1,13 @@
+; Using Simpson’s Rule, the integral of a function f between
+; a and b is approximated as
+; h/3 (y0 + 4y1 + 2y2 + 4y3 + 2y4 + · · · + 2yn-2 + 4yn-1 + yn);
+; where h = (b - a)=n, for some even integer n, and yk = f (a + kh).
+; (Increasing n increases the accuracy of the approximation.) Define a
+; procedure that takes as arguments f , a, b, and n and returns the
+; value of the integral, computed using Simpson’s Rule. Use your procedure
+; to integrate cube between 0 and 1 (with n = 100 and n = 1000), and compare
+; the results to those of the integral procedure shown above
+
 #lang sicp
 
 (define (sum term a next b)
